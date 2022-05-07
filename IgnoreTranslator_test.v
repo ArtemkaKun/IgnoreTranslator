1782fb4992e1bd26c8a5db4c98c93daf2c48b63c
Artem Yurchenko <zumikq78@gmail.com>
2022-05-07 17:15:18 +0200

    IgnoreTranslator_test - test_convert_complexs_gitignore_rule_to_ignore_conf_rule code was reimplemented to simplify testing logic

--- IgnoreTranslator_test.v
+++ IgnoreTranslator_test.v
@@ -26,3 +26,17 @@ fn test_convert_gitignore_rule_to_ignore_conf_rule() {
 
 	assert actual_results == expected_results
 }
+
+fn test_convert_complexs_gitignore_rule_to_ignore_conf_rule() {
+	rule := '/[Aa]ssets/[Aa]ddressable[Aa]ssets[Dd]ata/*/*.bin*'
+	expected_result := '/Assets/AddressableAssetsData/*/*.bin*\n/Assets/AddressableAssetsdata/*/*.bin*\n/Assets/AddressableassetsData/*/*.bin*\n/Assets/Addressableassetsdata/*/*.bin*\n/Assets/addressableAssetsData/*/*.bin*\n/Assets/addressableAssetsdata/*/*.bin*\n/Assets/addressableassetsData/*/*.bin*\n/Assets/addressableassetsdata/*/*.bin*\n/assets/AddressableAssetsData/*/*.bin*\n/assets/AddressableAssetsdata/*/*.bin*\n/assets/AddressableassetsData/*/*.bin*\n/assets/Addressableassetsdata/*/*.bin*\n/assets/addressableAssetsData/*/*.bin*\n/assets/addressableAssetsdata/*/*.bin*\n/assets/addressableassetsData/*/*.bin*\n/assets/addressableassetsdata/*/*.bin*'
+
+	actual_result := convert_gitignore_rule_to_ignore_conf_rule(rule)
+
+	splited_actual_result := actual_result.split('\n')
+	splited_expected_result := expected_result.split('\n')
+
+	for result in splited_actual_result {
+		assert result in splited_expected_result
+	}
+}
